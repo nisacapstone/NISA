4b62f7c5a609cb66d03644f968cd71dc49fd9c4b
Saphia Mohamed <100108435+safa1904@users.noreply.github.com>
2024-10-28 23:36:46 -0600

    add the navigation page and made it click able to go back and forth between the pages

--- obj/Debug/net8.0/NISA.AssemblyInfo.cs
+++ obj/Debug/net8.0/NISA.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NISA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a44388b88e40eac5ac49bf2e4c483f68e7c68fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfc0e07b463cea3cd22275153939294a62901e96")]
 [assembly: System.Reflection.AssemblyProductAttribute("NISA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NISA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
